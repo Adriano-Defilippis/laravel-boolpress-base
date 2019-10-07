4b2a0f56d7d30b425956be0b5896bdb3b8376232
Adriano-Defilippis <ciprianoadrianodefilippis@gmail.com>
2019-10-07 13:26:33 +0200

    Creata tabella post con faker e stampata a schermo

---
+++ resources/views/page/post_index.blade.php
@@ -0,0 +1,25 @@
+@extends('layout.layout_index')
+@section('content')
+
+<aside class="">
+  <a href="#">NEW POST</a>
+</aside>
+  <div class="wrapper-box">
+    @foreach ($posts as $post)
+      <div class="box">
+        <span>Titolo Post:</span>
+        <p>{{ $post -> title }}</p>
+        <span>Descrizione Post:</span>
+        <p>{{ $post -> desc }}</p>
+        <span>Autore del Post:</span>
+        <p>{{ $post -> author }}</p>
+        <div class="actions">
+          <a href="#">EDIT</a>
+          <a href="#">DELETE</a>
+        </div>
+      </div>
+    @endforeach
+
+  </div>
+
+@endsection
